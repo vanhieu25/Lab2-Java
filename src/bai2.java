81b3dc3dd2ffcc0505125dc8418217f20caf56e1
Van_Hieu <20230961@eaut.edu.vn>
2024-10-02 12:40:00 +0700

    init commit

--- src/bai2.java
+++ src/bai2.java
@@ -7,32 +7,29 @@ public class bai2 {
         System.out.println("Nhap vao so nguyen a: ");
         int a = sc.nextInt();
         System.out.println("Nhap vao so nguyen b: ");
-        int b= sc.nextInt();
+        int b = sc.nextInt();
         System.out.println("Nhap vao so nguyen c: ");
         int c = sc.nextInt();
-        if (a==0){
-            if (b==0 && c==0){
+        if (a == 0) {
+            if (b == 0 && c == 0) {
                 System.out.println("Phuong trinh co vo so nghiem");
-            } else if (b==0 && c!=0) {
+            } else if (b == 0 && c != 0) {
                 System.out.println("Phuong trinh vo nghiem");
+            } else {
+                System.out.println("Phuong trinh co nghiem la:" + -b / c);
             }
-            else{
-                System.out.println("Phuong trinh co nghiem la:"+ -b/c);
-            }
-        }
-        else{
-            double delta = ((b^2)-(4*a*c));
-            if (delta <0){
+        } else {
+            double delta = ((b ^ 2) - (4 * a * c));
+            if (delta < 0) {
                 System.out.println("Phuong trinh vo nghiem");
-            } else if (delta==0) {
-                System.out.println("Phuong trinh co nghiem kep la: "+ (-b/(2*a)));
-            }
-            else {
-                double x1 = ((-b+ Math.sqrt(delta))/2*a);
-                double x2 = ((-b- Math.sqrt(delta))/2*a);
+            } else if (delta == 0) {
+                System.out.println("Phuong trinh co nghiem kep la: " + (-b / (2 * a)));
+            } else {
+                double x1 = ((-b + Math.sqrt(delta)) / 2 * a);
+                double x2 = ((-b - Math.sqrt(delta)) / 2 * a);
                 System.out.println("Phuong trinh co 2 nghiem la: ");
                 System.out.println("X1 = " + x1);
-                System.out.println("X2 = "+ x2);
+                System.out.println("X2 = " + x2);
             }
         }
     }
